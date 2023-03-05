df9893143fec1f5da863aeacd7270baeb4df2004
aPixbob <124712359+aPixbob@users.noreply.github.com>
2023-03-06 01:04:20 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,66 +1,55 @@
-# python3
-
+# 221RDB216 Roberts Kārlis Kaudze grupa 4
 import sys
 import threading
 import numpy
 
 
-def compute_augst(n, parents):
-    
-    augst = numpy.zeros(n)
-
-    def kads_augst(node):
-        if augst[node] != 0:
-            return augst[node]
-
-        if parents[node] == -1:
-            augst[node] = 1
-
-        else:
-            augst[node] = 1 + kads_augst(parents[node])
-
-        return augst[node]
-
-    max_augst = 0
-
- 
-
-
+def compute_height(n, vecaks):
+    # tree creation
+    bern = [[] for _ in range(n)]
     for i in range(n):
-        max_augst = max(max_augst, kads_augst(i))
+        vecaks1 = vecaks[i]
+        if vecaks1 == -1:
+            sakn = i
+        else:
+            bern[vecaks1].append(i)
 
-    return max_augst
+    # augst for tree
+    def compute_depth(node):
+        if not bern[node]:
+            return 1
+        max_depth = 0
+        for bern0 in bern[node]:
+            depth = compute_depth(bern0)
+            max_depth = max(max_depth, depth)
+        return max_depth + 1
+
+    return compute_depth(sakn)
 
 
 def main():
-
     input_type = input()
 
-    if input_type == "F":
-        file = input()
-        if "a" in file:
-            print("wrong file name")
-            return
-        try:
-            with open("folder/" + file, "r", encoding="utf-8") as f:
-                n = int(f.readline())
-                parents = numpy.array(list(map(int, f.readline().split())))
-        except FileNotFoundError:
-            print("Wrong file")
-            return
-
-        print(compute_augst(n, parents))
-
-
-    elif input_type == "I":
+    if "I" in input_type:
         n = int(input())
-        parents = numpy.array(list(map(int, input().split())))
-        print(compute_augst(n, parents))
-        pass
+        vecaks = list(map(int, input().split()))
+        height = compute_height(n, vecaks)
+        print(height)
+    elif "F" in input_type:
+        filename = input()
+        with open("test/" + filename, "r") as f:
+            n = int(f.readline())
+            vecaks = list(map(int, f.readline().split()))
+            height = compute_height(n, vecaks)
+            print(height)
+    else:
+        print("incorrecto!")
+        exit()
 
 
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27)
+# In Python, the default limit on recursion depth is rather low,
+# so raise it here for this problem. Note that to take advantage
+# of bigger stack, we have to launch the computation in a new thread.
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)  # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
