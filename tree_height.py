6a5801229a58741003dd5cf078dc404206b038df
aPixbob <124712359+aPixbob@users.noreply.github.com>
2023-03-06 00:55:05 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -5,49 +5,62 @@ import threading
 import numpy
 
 
-class Node:
-    def __init__(self):
-        self.parent = None
-        self.children = []
-
-def build_tree(n, parents):
-    # Create nodes
-    nodes = [Node() for i in range(n)]
+def compute_augst(n, parents):
     
-    # Build tree
-    for i in range(n):
-        parent = parents[i]
-        if parent == -1:
-            root = nodes[i]
+    augst = numpy.zeros(n)
+
+    def kads_augst(node):
+        if augst[node] != 0:
+            return augst[node]
+
+        if parents[node] == -1:
+            augst[node] = 1
+
         else:
-            nodes[parent].children.append(nodes[i])
-            nodes[i].parent = nodes[parent]
-    
-    return root
+            augst[node] = 1 + kads_augst(parents[node])
 
-def compute_height(root):
-    # Base case: leaf node
-    if not root.children:
-        return 0
-    
-    # Recursive case: find maximum depth of children
-    max_depth = 0
-    for child in root.children:
-        max_depth = max(max_depth, compute_height(child))
-    
-    return max_depth + 1
+        return augst[node]
+
+    max_augst = 0
+
+ 
+
+
+    for i in range(n):
+        max_augst = max(max_augst, kads_augst(i))
+
+    return max_augst
+
 
 def main():
-    # Read input
-    n = int(input())
-    parents = list(map(int, input().split()))
-    
-    # Build tree
-    root = build_tree(n, parents)
-    
-    # Compute and output height
-    print(compute_height(root))
 
-if __name__ == '__main__':
-    sys.setrecursionlimit(10**7)
-    main()
+    input_type = input()
+
+    if input_type == "F":
+        file = input()
+        if "a" in file:
+            print("wrong file name")
+            return
+        try:
+            with open("folder/" + file, "r", encoding="utf-8") as f:
+                n = int(f.readline())
+                parents = numpy.array(list(map(int, f.readline().split())))
+        except FileNotFoundError:
+            print("Wrong file")
+            return
+
+        print(compute_augst(n, parents))
+
+
+    elif input_type == "I":
+        n = int(input())
+        parents = numpy.array(list(map(int, input().split())))
+        print(compute_augst(n, parents))
+        pass
+
+
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
+threading.Thread(target=main).start()
+main()
+# print(numpy.array([1,2,3]))
